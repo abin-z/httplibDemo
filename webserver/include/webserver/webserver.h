7c79fb20368361eebc1a489cd15f9c38cd3712fd
abin <abin.z@outlook.com>
2025-10-24 23:11:33 +0800

    feat: 添加webserver文件夹

---
+++ webserver/include/webserver/webserver.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <mutex>
+#include <string>
+#include <unordered_set>
+
+#include "httplib.h"
+
+class WebServer
+{
+  public:
+    WebServer(const std::string& ip, int port);
+    ~WebServer();
+
+    void run();
+    void stop();
+
+  private:
+    std::string ip_;
+    int port_;
+
+    httplib::Server svr_;
+    std::unordered_set<std::string> sessions_;
+    std::mutex mtx_;
+
+    // 提取 cookie 中的 token
+    std::string extract_token_from_cookie(const std::string& cookie);
+};
